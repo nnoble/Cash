a25d9ba88f934a58fb682233f2f6173c0c632840
Nick Noble <nnoble@me.com>
2015-01-09 15:35:03 -0600

    Initial Commit

---
+++ Cash/Cash.h
@@ -0,0 +1,19 @@
+//
+//  Cash.h
+//  Cash
+//
+//  Created by Nick Noble on 1/9/15.
+//  Copyright (c) 2015 NickNoble. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cash.
+FOUNDATION_EXPORT double CashVersionNumber;
+
+//! Project version string for Cash.
+FOUNDATION_EXPORT const unsigned char CashVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cash/PublicHeader.h>
+
+
